6183b70114ebe5c85143abeefec9d3cfde77a578
Max Mulatz <max@bitcrowd.net>
2024-06-10 10:37:28 +0200

    Fix typo

--- helpers.py
+++ helpers.py
@@ -6,11 +6,11 @@ from dotenv import dotenv_values
 def load_env():
     attrs = dotenv_values()
 
-    COCEBASE_PATH_ABS = os.path.abspath(attrs['COCEBASE_PATH'])
-    VECTOR_DB_DIR_NAME = os.path.basename(COCEBASE_PATH_ABS) + "_vector_db"
-    VECTOR_DB_PATH = os.path.join(os.path.dirname(COCEBASE_PATH_ABS), VECTOR_DB_DIR_NAME)
+    CODEBASE_PATH_ABS = os.path.abspath(attrs['CODEBASE_PATH'])
+    VECTOR_DB_DIR_NAME = os.path.basename(CODEBASE_PATH_ABS) + "_vector_db"
+    VECTOR_DB_PATH = os.path.join(os.path.dirname(CODEBASE_PATH_ABS), VECTOR_DB_DIR_NAME)
 
-    attrs['CODEBASE_PATH'] = COCEBASE_PATH_ABS
+    attrs['CODEBASE_PATH'] = CODEBASE_PATH_ABS
     attrs['VECTOR_DB_PATH'] = VECTOR_DB_PATH
     attrs['CODE_SUFFIXES'] = list(map(str.strip, attrs['CODE_SUFFIXES'].split(",")))
 
